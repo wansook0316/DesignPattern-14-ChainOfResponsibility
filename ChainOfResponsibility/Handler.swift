a158a3e0bf29368b3d860e38cfc440a43efb47b2
wansook0316 <wansook0316@gmail.com>
2023-01-16 13:01:04 +0900

    create: Handler

---
+++ ChainOfResponsibility/Handler.swift
@@ -0,0 +1,39 @@
+//
+//  Handler.swift
+//  ChainOfResponsibility
+//
+//  Created by Choiwansik on 2023/01/16.
+//
+
+import Foundation
+
+internal protocol Handler {
+
+    var nextHandler: Handler? { get set }
+
+    func setNext(handler: Handler) -> Handler
+
+    func run(url: String)
+
+    func process(url: String)
+
+}
+
+extension Handler {
+
+    internal mutating func setNext(handler: Handler) -> Handler {
+        self.nextHandler = handler
+        return handler
+    }
+
+    internal func run(url: String) {
+        self.process(url: url)
+
+        if let nextHandler {
+            nextHandler.run(url: url)
+        }
+    }
+
+}
+
+
